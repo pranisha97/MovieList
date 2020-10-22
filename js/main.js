3da4989ec3cc8d163cee4f9ea4a871c4553c7161
pranishakatkam.87@gmail.com <pranishakatkam.87@gmail.com>
2020-10-22 19:39:33 +0530

    commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+
+(function($) {
+
+	
+  	$(window).load(function() {
+    	$("#status").fadeOut("slow"); 
+    	$("#preloader").delay(500).fadeOut("slow").remove();      
+
+  	}) 
+  	setTimeout(function() {
+   	$('#page-title h1').fitText(1, { minFontSize: '38px', maxFontSize: '54px' });
+
+  	}, 100);
+   $(window).on('scroll', function() {
+
+		var h = $('header').height();
+		var y = $(window).scrollTop();
+      var header = $('#main-header');
+
+	   if ((y > h + 30 ) && ($(window).outerWidth() > 768 ) ) {
+	      header.addClass('opaque');
+	   }
+      else {
+         if (y < h + 30) {
+            header.removeClass('opaque');
+         }
+         else {
+            header.addClass('opaque');
+         }
+      }
+
+	});
+
+  	$('.smoothscroll').on('click', function (e) {
+	 	
+	 	e.preventDefault();
+
+   	var target = this.hash,
+    	$target = $(target);
+
+    	$('html, body').stop().animate({
+       	'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+      	window.location.hash = target;
+      });
+
+  	});
+	var sections = $("section"),
+	navigation_links = $("#nav-wrap a");
+
+	if($("body").hasClass('homepage')) {
+
+		sections.waypoint( {
+
+	      handler: function(event, direction) {
+
+			   var active_section;
+
+				active_section = $(this);
+				if (direction === "up") active_section = active_section.prev();
+
+				var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+
+	         navigation_links.parent().removeClass("current");
+				active_link.parent().addClass("current");
+
+			},
+			offset: '25%'
+		});
+
+	}
+
+   $('form#contactForm button.submit').on('click', function() {
+
+      $('#image-loader').fadeIn();
+      $.ajax({
+
+	      type: "POST",
+	      success: function(msg) {
+
+               $('#image-loader').fadeOut();
+            
+	      }
+
+      });
+      return false;
+   });
+	var pxShow = 300; 
+	var fadeInTime = 400; 
+	var fadeOutTime = 400;
+	var scrollSpeed = 300; 
+	jQuery(window).scroll(function() {
+
+		if (jQuery(window).scrollTop() >= pxShow) {
+			jQuery("#go-top").fadeIn(fadeInTime);
+		} else {
+			jQuery("#go-top").fadeOut(fadeOutTime);
+		}
+
+	}); 
+
+})(jQuery);
